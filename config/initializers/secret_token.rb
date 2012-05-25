1d9afb576291f9a0c4b35190cf2e07dc7313270a
Bruno Assis <brunoassis@gmail.com>
2012-05-25 00:08:39 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sitieitor::Application.config.secret_token = 'c5803ba50866e0abb1f3376ab1584576c5ef553d4d66dbe2b90ecafb330d7f9858755528d3257d81bae4171e1e5d36d502533cc1ea2566ca937685ed828c09c8'
